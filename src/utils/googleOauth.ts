e69a7ce5b2ddd7bdf42fa5ce3eac04e477caf1ab
Ayon <ayonsharma1999@gmail.com>
2023-01-17 10:24:04 +0530

    recent changes

--- src/utils/googleOauth.ts
+++
@@ -1,5 +0,0 @@
-// export const gapi_creds = {
-//     googleClientID: "1093521316839-q6qi4gtd5ga7vscagfbbfuqvrm21rqlr.apps.googleusercontent.com",
-//     googleClientSecret: "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",
-//     googleOauthRedirectURL: "https://9127-49-37-106-75.in.ngrok.io/api/users/google"
-// }
